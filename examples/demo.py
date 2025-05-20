7d0a1f9b7d92b5315cd32bca3ddd55f54db21990
Quinten Teusink <17791@osgh.nl>
2025-05-20 12:04:42 +0200

    Update 2

--- examples/demo.py
+++ examples/demo.py
@@ -7,11 +7,12 @@ import colorutilsqt
 
 hex_color = "#3FAAFF"
 h, s, l = colorutilsqt.converter.hex_to_hsl(hex_color)
-print(f"HSL van {hex_color}: {h}, {s}%, {l}%")
+print(f"HSL of {hex_color}: {h}, {s}%, {l}%")
 
 new_hex = colorutilsqt.converter.hsl_to_hex(h, s, l)
-print(f"Terug naar HEX: {new_hex}")
+print(f"Back to HEX: {new_hex}")
 
 r, g, b = (63, 170, 255)
 colorutilsqt.terminal.color_block_print(r, g, b)
-colorutilsqt.terminal.color_print("Hallo in kleur!", r, g, b)
+colorutilsqt.terminal.color_print("Hello in color!", r, g, b)
+colorutilsqt.terminal.fading_print("Lorem Ipsum", (255, 0, 20), (40, 60, 200))
