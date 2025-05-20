2d3cc7ad340e95fbe05e344acab5665439bc1160
Quinten Teusink <17791@osgh.nl>
2025-05-20 22:08:54 +0200

    Pushed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colorutilsqt',
-    version='0.1.2',
+    version='0.1.3',
     author='Quinten Teusink',
     description='A simple library to convert and print colors in terminal',
     long_description=open('README.md', encoding='utf-8').read(),
