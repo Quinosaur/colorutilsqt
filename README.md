fdfe0c25c8b1949e4abb6e90d8a6fd0f7fb68e39
Quinten Teusink <17791@osgh.nl>
2025-05-20 22:05:04 +0200

    Impoved the moving_fade() fanction.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 **Colorutilsqt** is a simple and lightweight Python library for working with colors. It provides easy conversion between HEX, RGB, and HSL formats, as well as utilities for printing colored text or blocks in the terminal.
 
+If using the moving_fade() function, make sure to add colorutilsqt.enable_safe_print() at the top of your code, to make sure the code runs smoothly.
+
 ## Installation
 
 ```bash
--- colorutilsqt/__init__.py
+++ colorutilsqt/__init__.py
@@ -14,3 +14,25 @@ from .terminal import (
     color_block_print,
     fading_print,
 )
+
+import builtins
+import threading
+
+print_lock = threading.Lock()
+
+def safe_print(*args, **kwargs):
+    """Thread-safe print"""
+    with print_lock:
+        builtins._original_print(*args, **kwargs)
+
+def enable_safe_print():
+    """Vervangt de globale print-functie door een thread-safe versie"""
+    if not hasattr(builtins, "_original_print"):
+        builtins._original_print = builtins.print
+        builtins.print = safe_print
+
+def disable_safe_print():
+    """Herstelt de originele print-functie"""
+    if hasattr(builtins, "_original_print"):
+        builtins.print = builtins._original_print
+        del builtins._original_print
