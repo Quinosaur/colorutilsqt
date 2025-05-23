8a48dce93eb78409874abc0e5c54bdad230ebc04
Quinten Teusink <17791@osgh.nl>
2025-05-23 12:59:13 +0200

    Upgraded color_print() function.

--- colorutilsqt/terminal.py
+++ colorutilsqt/terminal.py
@@ -1,8 +1,8 @@
 def color_block_print(r, g, b):
     print(f"\033[48;2;{r};{g};{b}m   \033[0m RGB({r},{g},{b})")
 
-def color_print(text, r, g, b):
-    print(f"\033[38;2;{r};{g};{b}m{text}\033[0m")
+def color_print(text, r, g, b, **kwargs):
+    print(f"\033[38;2;{r};{g};{b}m{text}\033[0m", **kwargs)
 
 
 def fading_print(text, start_rgb, end_rgb):
